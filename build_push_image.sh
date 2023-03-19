d956a4c58a8e50dc7230e916e399112955e6c269
Yohanes Liwun <yohanes.liwun@gmail.com>
2023-03-19 18:37:54 +0700

    from build push image script

--- build_push_image.sh
+++ build_push_image.sh
@@ -9,11 +9,3 @@ docker tag item-app:v1 ghcr.io/footgraph/a433-microservices/item-app:v1
 echo $CR_PAT | docker login ghcr.io -u footgraph --password-stdin
 
 docker push ghcr.io/footgraph/a433-microservices/item-app:v1
-
-docker compose up
-
-docker compose logs > log.txt
-
-git add .
-git commit -m "from build push image script"
-git push
